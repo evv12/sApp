3f9e2071069425efc607d806367fa489e73be04a
kate <evv@microting.com>
2013-01-04 12:00:53 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SApp::Application.config.secret_token = 'aa63a73a961421a229a821c23c1dc64c3b2669b6eae51d23e1b3e563e11ed05b49907a742cb517eaf4471717492a821474f394d2463d402423c9f16fe1a3c431'
